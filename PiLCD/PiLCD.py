613470927f749d113039f6b6182e0ae0add940f7
edward sharp <edward@edwardsharp.net>
2014-04-14 22:23:00 -0400

    PiLCD fixupz!

--- PiLCD/PiLCD.py
+++ PiLCD/PiLCD.py
@@ -3,7 +3,7 @@
 from time import sleep
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
-import subprocess, glob, os
+import subprocess, glob, os, commands
 
 # Initialize the LCD plate.  Should auto-detect correct I2C bus.  If not,
 # pass '0' for early 256 MB Model B boards or '1' for all later versions
@@ -14,11 +14,11 @@ lcd.clear()
 lcd.message("HELLO!\nMAKE SELECTION")
 
 # Poll buttons, display message & set backlight accordingly
-btn = ((lcd.LEFT  , 'LEFT 4'  ),
-       (lcd.UP    , 'UP 3'    ),
-       (lcd.DOWN  , 'DOWN 2'  ),
-       (lcd.RIGHT , 'RIGHT 1' ),
-       (lcd.SELECT, 'SELECT 0'))
+btn = ((lcd.LEFT  , '' ),
+       (lcd.UP    , '' ),
+       (lcd.DOWN  , '' ),
+       (lcd.RIGHT , '' ),
+       (lcd.SELECT, '' ))
 
 def mySleep(amt=0.5):
   sleep(amt)
@@ -26,6 +26,8 @@ def mySleep(amt=0.5):
 LOCK_OFFSET = 0
 SCROLL_LOCK = False
 IS_FLASHING = False
+BOX_IP = commands.getoutput("/home/pi/myip.sh")
+
 def myScrollDisplayLeft():
   global SCROLL_LOCK, LOCK_OFFSET
   if SCROLL_LOCK:
@@ -43,8 +45,10 @@ def myScrollDisplayLeft():
 IDX = 0
 FUNCTIONZ = [['lxterminal', 'open .sh filez'], 
              ['pure data', 'open .pd filez'],
-             ['omxplayer', ' open .mov filez'], 
-             ['shutdown', 'shutdown NOW']]
+             ['omxplayer', 'open .mov filez'], 
+             ['txt\'r', 'show .txt filez'],
+             ['shutdown', 'shutdown NOW'],
+             ['ipconfig', BOX_IP]]
 
 def setNextFunc():
   global IDX, FUNCTIONZ
@@ -79,8 +83,19 @@ for root, dirs, files in os.walk('/home/pi'):
     if file.endswith('.mov'):
       SUB_FUNCTIONZ02.append(['omxplayer', os.path.join(root, file)])
 
+SUB_IDX03 = -1
+SUB_FUNCTIONZ03 = []
+for root, dirs, files in os.walk('/home/pi'):
+  for file in files:
+    if file.endswith('.txt'):
+      SUB_FUNCTIONZ03.append(os.path.join(root, file))
+
 def setNextSubFuncUp():
-  global IDX, SUB_IDX00, SUB_FUNCTIONZ00, SUB_IDX01, SUB_FUNCTIONZ01, SUB_IDX02, SUB_FUNCTIONZ02
+  global IDX, \
+         SUB_IDX00, SUB_FUNCTIONZ00, \
+         SUB_IDX01, SUB_FUNCTIONZ01, \
+         SUB_IDX02, SUB_FUNCTIONZ02, \
+         SUB_IDX03, SUB_FUNCTIONZ03
   #TODO: consider FUNCTIONZ
   if IDX is 0:
     SUB_IDX00 = SUB_IDX00 + 1
@@ -98,11 +113,20 @@ def setNextSubFuncUp():
       SUB_IDX02 = -1
     sub_string = SUB_FUNCTIONZ02[SUB_IDX02][0] + '\n' + SUB_FUNCTIONZ02[SUB_IDX02][1]
   elif IDX is 3:
+    SUB_IDX03 = SUB_IDX03 + 1
+    if SUB_IDX03 > len(SUB_FUNCTIONZ03) - 1:
+      SUB_IDX03 = -1
+    sub_string = SUB_FUNCTIONZ03[SUB_IDX03]
+  elif IDX is 4:
     sub_string = 'shutdown -h now'
   return sub_string
 
 def setNextSubFuncDown():
-  global IDX, SUB_IDX00, SUB_FUNCTIONZ00, SUB_IDX01, SUB_FUNCTIONZ01, SUB_IDX02, SUB_FUNCTIONZ02
+  global IDX, \
+         SUB_IDX00, SUB_FUNCTIONZ00, \
+         SUB_IDX01, SUB_FUNCTIONZ01, \
+         SUB_IDX02, SUB_FUNCTIONZ02, \
+         SUB_IDX03, SUB_FUNCTIONZ03
   #TODO: consider FUNCTIONZ
   if IDX is 0:
     SUB_IDX00 = SUB_IDX00 - 1
@@ -120,47 +144,71 @@ def setNextSubFuncDown():
       SUB_IDX02 = len(SUB_FUNCTIONZ02) - 1
     sub_string = SUB_FUNCTIONZ02[SUB_IDX02][0] + '\n' + SUB_FUNCTIONZ02[SUB_IDX02][1]
   elif IDX is 3:
+    SUB_IDX03 = SUB_IDX03 - 1
+    if SUB_IDX03 < 0:
+      SUB_IDX03 = len(SUB_FUNCTIONZ03) - 1
+    sub_string = SUB_FUNCTIONZ03[SUB_IDX03]
+  elif IDX is 4:
     sub_string = 'shutdown -h now'
   return sub_string  
 
 def getCurrentSubFunc():
-  global IDX, SUB_IDX00, SUB_FUNCTIONZ00, SUB_IDX01, SUB_FUNCTIONZ01, SUB_IDX02, SUB_FUNCTIONZ02
+  global IDX, \
+         SUB_IDX00, SUB_FUNCTIONZ00, \
+         SUB_IDX01, SUB_FUNCTIONZ01, \
+         SUB_IDX02, SUB_FUNCTIONZ02, \
+         SUB_IDX03, SUB_FUNCTIONZ03
   if IDX is 0:
     sub_string = SUB_FUNCTIONZ00[SUB_IDX00][0] + ' ' + SUB_FUNCTIONZ00[SUB_IDX00][1]
   elif IDX is 1:
-    sub_string = SUB_FUNCTIONZ01[SUB_IDX01][0] + ' ' + SUB_FUNCTIONZ01[SUB_IDX01][1]
+    sub_string = SUB_FUNCTIONZ01[SUB_IDX01][0] + ' ' + SUB_FUNCTIONZ01[SUB_IDX01][1] +' &'
   elif IDX is 2:
     sub_string = SUB_FUNCTIONZ02[SUB_IDX02][0] + ' ' + SUB_FUNCTIONZ02[SUB_IDX02][1]
   elif IDX is 3:
+    sub_string = SUB_FUNCTIONZ03[SUB_IDX03]
+  elif IDX is 4:
     sub_string = 'shutdown -h now'
   return sub_string
 
+def showFile(fname):
+  with open(fname) as f:
+    while True:
+      #kindof sloppy arg passed here...
+      if lcd.buttonPressed(0):
+        break
+      c = f.read(32)
+      top, bottom = c[:16], c[16:]
+      lcd.clear()
+      lcd.message(top+'\n'+bottom)
+      mySleep(6)
 
 """ SELECT BUTTON """
 def makeSelection(button):
-  global lcd, prev, SUB_IDX01, SUB_FUNCTIONZ01, SUB_IDX02, SUB_FUNCTIONZ02
+  global lcd, prev, IDX, \
+         SUB_IDX00, SUB_FUNCTIONZ00, \
+         SUB_IDX01, SUB_FUNCTIONZ01, \
+         SUB_IDX02, SUB_FUNCTIONZ02, \
+         SUB_IDX03, SUB_FUNCTIONZ03
   lcd.clear()
-  mySleep(0.2)
   lcd.message(prev[1])
-  mySleep(0.2)
+  mySleep(0.1)
   lcd.clear()
+  mySleep(0.1)
   lcd.message(prev[1])
-  mySleep(0.2)
+  mySleep(0.1)
   lcd.clear()
-  mySleep(0.2)
-  lcd.message(prev[1])
-  mySleep(0.2)
-  lcd.message(prev[1])
-  mySleep(0.2)
-  lcd.clear()
-  mySleep(0.2)
+  mySleep(0.1)
   lcd.message(prev[1])
   #end flash
   lcd.clear()
   lcd.message(getCurrentFunc()+'\n'+getCurrentSubFunc())
   print getCurrentSubFunc()
-  #
-  subprocess.call(getCurrentSubFunc(), shell=True)
+  
+  if IDX is 3:
+    showFile(getCurrentSubFunc())
+  else:
+    #shell=True enables this function to accept stringz
+    subprocess.call(getCurrentSubFunc(), shell=True)
   lcd.clear()
   lcd.message('called:\n'+getCurrentSubFunc())
   prev = -1
